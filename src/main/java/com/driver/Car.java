e6832ffed94ba5ff0e71914f39d2dfa3556ba61d
imsidthakur <106941477+imsidthakur@users.noreply.github.com>
2024-10-05 11:13:58 +0530

    Update Car.java

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -17,7 +17,7 @@ public class Car extends Vehicle {
         this.gears  = gears;
         this.isManual = isManual;
         this.seats = seats;
-        this.currentGear = 1;
+        this.currentGear = 0;
     }
 
     public int getCurrentGear()
